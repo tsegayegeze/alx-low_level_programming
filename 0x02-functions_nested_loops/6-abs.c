e502013f5420b4354bb8c98c52a8d9ae86f433c0
tsegayegeze <tsegayegeze@gmail@com>
2022-03-16 21:41:36 +0300

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _abs - _abs function
+ *
+ * Description: the _abs function
+ * puts text to stout.
+ * @i: int i
+ * Return: 0
+ */
+
+int _abs(int i)
+{
+	return (abs(i));
+}
