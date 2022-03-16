46a2a6060ab79d29335e537d4239a84aba69dc8c
tsegayegeze <tsegayegeze@gmail@com>
2022-03-16 22:08:29 +0300

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * add - add function
+ *
+ * Description: the add function
+ * puts text to stout.
+ * @a: int a
+ * @b: int b
+ * Return: void
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
